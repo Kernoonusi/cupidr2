241e2de0a13d7d8ea4e845f5e348eba3d3cd5e7a
Kernoonusi <kernoonusi@gmail.com>
2024-02-24 11:13:35 +0500

    fix: chats is full width now

--- src/app/swipes/page.tsx
+++ src/app/swipes/page.tsx
@@ -1,7 +1,19 @@
+"use client";
+const recomendedUsers = [
+	{
+		name: "Jane",
+		image: "https://github.com/shadcn.png",
+	},
+	{
+		name: "John",
+		image: "https://github.com/shadcn.png",
+	},
+];
+
 export default function Swipes() {
-    return (
-        <main className="max-w-7xl mx-auto px-4">
-            <h1 className="text-3xl font-bold text-center">Swipes</h1>
-        </main>
-    );
+	return (
+		<main className="max-w-7xl mx-auto px-4">
+			<h1 className="text-3xl font-bold text-center">Swipes</h1>
+		</main>
+	);
 }
