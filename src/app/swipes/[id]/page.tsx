0b9b449ffaddba288c0d713d12495d0e9fd2b36c
Kernoonusi <kernoonusi@gmail.com>
2024-02-24 16:45:05 +0500

    fix: ui unglued from images

--- src/app/swipes/[id]/page.tsx
+++ src/app/swipes/[id]/page.tsx
@@ -8,44 +8,45 @@ import {
 	CarouselPrevious,
 } from "@/components/ui/carousel";
 import getSwipes from "@/app/api/swipes";
+import Image from "next/image";
 export default async function Swipes({ params: { id } }: { params: { id: number } }) {
 	const recomendedUser = await getSwipes(id);
 	return (
-		<main className="w-full mx-auto px-4 relative overflow-hidden">
-			<Carousel>
+		<main className="max-w-xl mx-auto px-4 relative overflow-hidden">
+			<Carousel className="bg-slate-200 rounded-3xl mt-4">
 				<CarouselContent>
-					{recomendedUser.photos.map((user) => (
-						<CarouselItem>
+					{recomendedUser.photos.map((photo) => (
+						<CarouselItem key={photo}>
 							<Card className="rounded-3xl">
 								<CardContent className="p-0">
-									<img src={user} alt={user} className="w-full h-full object-cover rounded-3xl" />
-									<div className="w-3/4 absolute bottom-32 left-14 text-white">
-										<h2 className="text-3xl">
-											{recomendedUser.name}, {recomendedUser.age}
-										</h2>
-										<p>{recomendedUser.description}</p>
-										<p className="flex items-center gap-1">
-											<span className="material-symbols-outlined">location_on</span>
-											{recomendedUser.location}
-										</p>
-									</div>
-									<Button
-										variant="outline"
-										size="icon"
-										className="absolute bottom-8 left-14 w-fit h-fit p-1 rounded-full bg-red-600 border-0">
-										<span className="material-symbols-outlined text-6xl">cancel</span>
-									</Button>
-									<Button
-										variant="outline"
-										size="icon"
-										className="absolute bottom-8 right-14 w-fit h-fit p-1 pb-0 rounded-full bg-blue-600 border-0">
-										<span className="material-symbols-outlined text-6xl">favorite</span>
-									</Button>
+									<Image src={photo} alt={photo} className="aspect-[9/16] object-cover rounded-3xl" width={900} height={1600} />
 								</CardContent>
 							</Card>
 						</CarouselItem>
 					))}
 				</CarouselContent>
+				<div className="w-3/4 absolute bottom-32 left-14 text-white">
+					<h2 className="text-3xl">
+						{recomendedUser.name}, {recomendedUser.age}
+					</h2>
+					<p>{recomendedUser.description}</p>
+					<p className="flex items-center gap-1">
+						<span className="material-symbols-outlined">location_on</span>
+						{recomendedUser.location}
+					</p>
+				</div>
+				<Button
+					variant="outline"
+					size="icon"
+					className="absolute bottom-8 left-14 w-fit h-fit p-1 rounded-full bg-red-600 border-0">
+					<span className="material-symbols-outlined text-6xl">cancel</span>
+				</Button>
+				<Button
+					variant="outline"
+					size="icon"
+					className="absolute bottom-8 right-14 w-fit h-fit p-1 pb-0 rounded-full bg-blue-600 border-0">
+					<span className="material-symbols-outlined text-6xl">favorite</span>
+				</Button>
 				<CarouselPrevious className="hidden md:block" />
 				<CarouselNext className="hidden md:block" />
 			</Carousel>
