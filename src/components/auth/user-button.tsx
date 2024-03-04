357b6c5395cd634df7fdb1273941f7ab2a6ccb90
Qwano <domocristalix@gmail.com>
2024-03-04 21:13:51 +0500

    feat: user button

--- src/components/auth/user-button.tsx
+++ src/components/auth/user-button.tsx
@@ -1,43 +1,67 @@
 'use client';
 
-import { LogOut, User, Settings } from 'lucide-react';
+import { LogOut, User, Settings, ChevronDown, Palette } from 'lucide-react';
+import Link from 'next/link';
+import { signOut } from 'next-auth/react';
+
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
+  DropdownMenuLabel,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
-
 import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar';
 import { useCurrentUser } from '@/hooks/use-current-user';
-import { signOut } from 'next-auth/react';
-import Link from 'next/link';
+import { ThemeSelect } from '@/components/theme-select';
 
 export const UserButton = () => {
   const user = useCurrentUser();
 
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger className="flex items-center space-x-4">
-        {/* <p className="font-mono text-lg">{user?.name}</p> */}
+      <DropdownMenuTrigger className="flex items-center space-x-2">
         <Avatar>
           <AvatarImage src={user?.image || ''} alt="User avatar" />
           <AvatarFallback className="bg-emerald-500">
             <User className="text-white" />
           </AvatarFallback>
         </Avatar>
+        <ChevronDown className="w-4 h-4 text-black dark:text-white" />
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
-        <DropdownMenuItem>
-          <Link href="/settings">
+        <DropdownMenuLabel>
+          <div className="flex justify-center items-center space-x-2">
+            <Avatar>
+              <AvatarImage src={user?.image || ''} alt="User avatar" />
+              <AvatarFallback className="bg-emerald-500">
+                <User className="text-white" />
+              </AvatarFallback>
+            </Avatar>
+            <div>
+              <p>{user?.name}</p>
+              <p className="text-xs text-slate-400">{user?.email}</p>
+            </div>
+          </div>
+        </DropdownMenuLabel>
+        <DropdownMenuItem asChild>
+          <Link href="/settings" className="w-full">
             <div className="flex">
               <Settings className="w-4 h-4 mr-2" />
               Settings
             </div>
           </Link>
         </DropdownMenuItem>
-        <DropdownMenuItem className="cursor-pointer" onClick={() => signOut()}>
-          <LogOut className="w-4 h-4 mr-2" />
+
+        <DropdownMenuLabel className="flex items-center space-x-4">
+          <div className="flex items-center">
+            <Palette className="w-4 h-4 mr-2" />
+            Theme:
+          </div>
+          <ThemeSelect />
+        </DropdownMenuLabel>
+        <DropdownMenuItem className="cursor-pointer bg-destructive/15" onClick={() => signOut()}>
+          <LogOut className="w-4 h-4 mr-2 text-destructive" />
           Logout
         </DropdownMenuItem>
       </DropdownMenuContent>
--- src/components/header/link-button.tsx
+++ src/components/header/link-button.tsx
@@ -1,26 +1,33 @@
-"use client";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
+'use client';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 export default function LinkButton({
-	children,
-	href,
-	passHref,
+  children,
+  href,
+  passHref,
 }: {
-	children: React.ReactNode;
-	href: string;
-	passHref: boolean;
+  children: React.ReactNode;
+  href: string;
+  passHref: boolean;
 }) {
-	const pathname = usePathname()?.split("/")[1];
-	return (
-		<>
-			<Link
-				href={href}
-				passHref={passHref}
-				className="flex w-max gap-4 px-3 py-4 transition items-center justify-center rounded-lg hover:text-primary dark:hover:text-primary">
-				{children}
-			</Link>
-			<input onChange={()=>{}} type="radio" placeholder="current-page" name="current-page" checked={"/" + pathname === href} className="hidden" />
-		</>
-	);
+  const pathname = usePathname()?.split('/')[1];
+  return (
+    <>
+      <Link
+        href={href}
+        passHref={passHref}
+        className="flex w-max gap-4 px-3 py-4 transition rounded-lg hover:text-primary dark:hover:text-primary">
+        {children}
+      </Link>
+      <input
+        onChange={() => {}}
+        type="radio"
+        placeholder="current-page"
+        name="current-page"
+        checked={'/' + pathname === href}
+        className="hidden"
+      />
+    </>
+  );
 }
