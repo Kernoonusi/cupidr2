f28d77f7d11d26e16f941b5f39e6dd9735a08f3b
Kernoonusi <kernoonusi@gmail.com>
2024-10-29 20:56:37 +0500

    change to bun, try to fix images

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    domains: ["www.dropbox.com", "dl.dropboxusercontent.com"],
     remotePatterns: [
       {
         protocol: "https",
